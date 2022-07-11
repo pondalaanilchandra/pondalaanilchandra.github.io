dc5053e455f9e26bc132d125f63f58829771ca2d
pondalaanilchandra <pondalaanilchandra.20.cse@anits.edu.in>
2022-07-11 13:23:14 +0530

    update

--- assignment/script.js
+++ assignment/script.js
@@ -2,12 +2,13 @@ var names=new Array();
 names[0]="Yaakov";
 names[1]="john";
 names[2]="jen";
-names[3]="anil";
-names[4]="chandra";
-names[5]="jyo";
-names[6]="kavitha";
-names[7]="manisha";
-names[8]="sunitha";
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
 for(var i=0;i< names.length;i++)
 {
     if(names[i].charAt(0)==='j'|| names[i].charAt(0)==='J')
